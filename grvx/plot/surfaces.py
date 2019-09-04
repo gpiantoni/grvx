8fa6b31b939e0ccdb94f3d44a787a67fef18c3c9
Gio at UMCU <github@gpiantoni.com>
2019-09-04 12:18:09 +0200

    plot surfaces as well

--- grvx/plot/surfaces.py
+++ grvx/plot/surfaces.py
@@ -1,6 +1,6 @@
-from wonambi.attr import Surf, Channels, Freesurfer
+from wonambi.attr import Channels, Freesurfer
 from wonambi.viz import Viz3
-from numpy import hstack, vstack, array, median
+from numpy import array, median
 from bidso import Electrodes
 from bidso import file_Core
 from bidso.utils import read_tsv
@@ -8,22 +8,23 @@ from bidso.find import find_in_bids
 
 from pathlib import Path
 
+from ..core.constants import PARAMETERS
+
 results_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/nipype/grvx/corr_fmri_ecog_summary/output/ecog')
-plots_dir = Path('/Fridge/users/giovanni/projects/grvx/derivatives/plots/surfaces')
 
-pvalue = 0.05  # TODO: PARAMETERS
+pvalue = PARAMETERS['corr']['pvalue']
 
 
-def plot_surfaces():
+def plot_surfaces(plots_dir):
     for results_path in results_dir.glob('*.tsv'):
         results = read_tsv(results_path)
 
         r = file_Core(results_path)
 
-        fs = Freesurfer(f'/Fridge/users/giovanni/projects/grvx/derivatives/freesurfer/sub-{r.subject}')
+        fs = Freesurfer(f'/Fridge/users/giovanni/projects/freesurfer/sub-{r.subject}')
         brain = fs.read_brain()
 
-        electrodes_file = find_in_bids(Path('/Fridge/users/giovanni/projects/grvx/subjects/'), subject=r.subject, acquisition='*alctmrregions', modality='electrodes')
+        electrodes_file = find_in_bids(Path('/Fridge/users/giovanni/projects/grvx/subjects/'), subject=r.subject, acquisition='*al', modality='electrodes')
         electrodes = Electrodes(electrodes_file)
 
         labels = electrodes.electrodes.get(map_lambda=lambda x: x['name'])
@@ -38,12 +39,12 @@ def plot_surfaces():
         if median(chan_xyz[:, 0]) > 0:
             v.add_surf(brain.lh)
             v.add_surf(brain.rh)
-            v._view.camera.azimuth = 120
+            v._view.camera.azimuth = 90
         else:
             v.add_surf(brain.rh)
             v.add_surf(brain.lh)
-            v._view.camera.azimuth = -120
+            v._view.camera.azimuth = -90
 
-        v._view.camera.elevation = 20
-        v.save(plots_dir / (r.subject + '.png'))
+        v._view.camera.elevation = 30
+        v.save(plots_dir / ('surfaces_' + r.subject + '.png'))
         v.close()
